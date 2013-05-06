27367047fc1cd1fed1f8606b42ee9f4bdf73febf
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2013-05-06 10:06:28 +0200

    Getting just the relevant REST services for a goal (thanks to @RubenVerborgh)

--- scenario1/rules_02/test.sh
+++ scenario1/rules_02/test.sh
@@ -6,4 +6,4 @@
 #java -jar ../Euler.jar --nope --pass server_image.n3 description_thumbnail.n3
 #java -jar ../../Euler.jar --help
 #java -jar ../../Euler.jar --nope --quick-answer agent_goal.n3 light_get.n3 light_post.n3
-java -jar ../../Euler.jar --nope --quick-answer light_together.n3 --query light_goal.n3
+java -jar ../../Euler.jar light_together.n3 --query light_goal.n3
