76e4d6f68defabc99ad524248b7e00f9ecb2c92e
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2013-05-08 18:26:25 +0200

    Spliting the 3rd iteration of light scenario in different rules to interpret result.txt more easily.

--- scenario1/rules_03/test.sh
+++ scenario1/rules_03/test.sh
@@ -1,9 +1,4 @@
 #! /bin/bash
 
-# http://notes.restdesc.org/2011/images/index.html
-
-#java -jar ../Euler.jar --nope --pass agent_image.n3 description_images.n3
-#java -jar ../Euler.jar --nope --pass server_image.n3 description_thumbnail.n3
-#java -jar ../../Euler.jar --help
-#java -jar ../../Euler.jar --nope --quick-answer agent_goal.n3 light_get.n3 light_post.n3
-java -jar ../../Euler.jar light_together.n3 --query light_goal.n3
+#java -jar ../../Euler.jar light_together.n3 --query light_goal.n3 # more difficult to interpret
+java -jar ../../Euler.jar facts.n3 light1_rule.n3 light2_rule.n3 temperature_rule.n3 unrelated_rule.n3 --query light_goal.n3
