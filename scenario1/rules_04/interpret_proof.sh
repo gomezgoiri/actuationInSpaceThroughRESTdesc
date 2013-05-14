7112689311836ee24b68660f1f31ec91f66343c8
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2013-05-14 16:25:36 +0200

    First steps on defining 'open/close blind' service

---
+++ scenario1/rules_04/interpret_proof.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+export PYTHONPATH=$PYTHONPATH:../ScnImpl
+python ../ScnImpl/wot2013/proofs/extract_info.py -i result.txt -o /tmp -e ../../
---
+++ scenario1/rules_04/test.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+#java -jar ../../Euler.jar light_together.n3 --query light_goal.n3 # more difficult to interpret
+java -jar ../../Euler.jar facts.n3 light1_rule.n3 light2_rule.n3 temperature_rule.n3 unrelated_rule.n3 --query light_goal.n3
