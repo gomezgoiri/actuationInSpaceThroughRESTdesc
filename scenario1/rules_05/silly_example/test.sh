c18fafbf248c6109b15a96f22aa1ee06357ec983
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2013-05-19 19:31:44 +0200

    Added silly example to understand better how rules and TSC could coexist

---
+++ scenario1/rules_05/silly_example/test.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+#java -jar ../../Euler.jar light_together.n3 --query light_goal.n3 # more difficult to interpret
+java -jar ../../../Euler.jar facts.n3 light3_rule.n3 --query light_goal.n3
