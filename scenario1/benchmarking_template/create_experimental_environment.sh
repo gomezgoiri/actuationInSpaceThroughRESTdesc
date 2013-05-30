861aeb6e0628db5c2d3f7f2c6918b67884783bf5
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2013-05-31 01:44:34 +0200

    Creating templates for the benchmarking

---
+++ scenario1/benchmarking_template/create_experimental_environment.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+export PYTHONPATH=$PYTHONPATH:../ScnImpl
+
+python ../ScnImpl/wot2013/evaluation/environment.py 
---
+++ scenario1/benchmarking_template/simulate_scenario.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+export PYTHONPATH=$PYTHONPATH:../ScnImpl
+
+python ../ScnImpl/wot2013/scenario.py -i $1 -e ../../
