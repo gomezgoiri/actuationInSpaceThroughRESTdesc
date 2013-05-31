8a7c225480fe5950b19064fa94f1244a7a5482bb
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2013-05-31 18:52:19 +0200

    Benchmarks are know launched for all the configuration files in the folder provider as parameter

--- scenario1/benchmarking_template/execute_benchmarks.sh
+++ scenario1/benchmarking_template/execute_benchmarks.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+
 export dataset_path=$1
 export default_unrel_graphs=1
 export default_unrel_rules=1
-export default_unrelated=1
+export default_not_feasible_rules=1
+
+
+# Uniformize dataset_path to have an slash at the end
+case "$dataset_path" in
+*/)
+    #echo "has slash"
+    ;;
+*)
+    #echo "doesn't have a slash"
+    export dataset_path="$dataset_path/"
+    ;;
+esac
+
+
+
 
 
 function get_config_file() {
@@ -11,7 +27,7 @@ function get_config_file() {
 }
 
 function get_result_file() {
-  result_file=$results_path"/r_$1_$2_$3.out"
+  result_file=$results_path"r_$1_$2_$3.out"
 }
 
 
@@ -25,11 +41,26 @@ results_path=$dataset_path"results"
 mkdir $results_path
 echo "Results path created in $results_path"
 
-for unrel_graph in 1 100
+
+
+for config_file in $dataset_path*.config # since I am not talking about a string but about a file/folder, without ""
 do
-  get_config_file $unrel_graph $default_unrel_rules $default_unrelated
-  #echo $config_file
-  get_result_file $unrel_graph $default_unrel_rules $default_unrelated
-  echo $result_file
+  #echo $confile
+  filename=$(basename "$config_file")
+  #extension="${filename##*.}"
+  filename="${filename%.*}"
+  #echo $filename
+  result_file=$results_path"/$filename.out"
+  echo "Processing $config_file > $result_file"
   `bash simulate_scenario.sh $config_file > $result_file 2>&1`
-done 
+done
+
+
+#for unrel_graph in 1 100
+#do
+#  get_config_file $unrel_graph $default_unrel_rules $default_not_feasible_rules
+#  echo $config_file
+#  get_result_file $unrel_graph $default_unrel_rules $default_not_feasible_rules
+#  echo $result_file
+#  `bash simulate_scenario.sh $config_file > $result_file 2>&1`
+#done
--- scenario1/benchmarking_template/process_results.sh
+++ scenario1/benchmarking_template/process_results.sh
@@ -1,9 +1,19 @@
 #! /bin/bash
 
 export PYTHONPATH=$PYTHONPATH:../ScnImpl
-
 #export folder=/tmp/tmp4uDskV/results/*
-export folder="$1/*"
+
+# Uniformize dataset_path to have an slash at the end
+case "$1" in
+*/)
+    #echo "has slash"
+    export folder="$1*"
+    ;;
+*)
+    #echo "doesn't have a slash"
+    export folder="$1/*"
+    ;;
+esac
 
 
 for result_file in $folder
